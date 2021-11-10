62ed21cc66750a7ed7d5fab5ea103d2c3ee47410
DESKTOP-7S7GQNM\DELL <saxena.manvi369@gmail.com>
2021-11-11 01:01:29 +0530

    changes added

--- MultiThreading.java
+++ MultiThreading.java
@@ -8,17 +8,30 @@
 //         th.start();
 //     }
 // }
-
-public class MultiThreading implements Runnable{
-
-    @Override
-    public void run() {
-        System.out.println("runnable interface");
-        
-    }
-    public static void main(String[] args) {
-               MultiThreading mt=new MultiThreading();
-               Thread  th=new Thread(mt);
-               th.start();      
+// Performing single task from single thread
+class Test extends Thread{
+        public void run(){
+            System.out.println("single thread task");
+        }
+       
+}
+// Performing single task from multiple thread
+class Test2 extends Thread{
+    public void run(){
+        System.out.println("single task from multiple thread");
     }
 }
+
+
+public class MultiThreading {
+    public static void main(String[] args) {
+        Test t=new Test();
+        t.start();
+        Test2 t2=new Test2();
+        t2.start();
+        Test2 t22=new Test2();
+        t22.start();
+
+    }
+}
+
